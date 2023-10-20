cc23de5d5a868e0658e4f491f673df50109e7011
Gellde <gelldesmodus@gmail.com>
2023-10-20 17:07:24 +0300

    js images&animation

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import images from "./modules/images";
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -16,4 +17,5 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    images();
 });
